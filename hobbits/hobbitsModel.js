538f6251cad68a8dc80c636b3c6a0c5dd7d9c323
Pete James <petejames@petes-air.lan>
2020-04-30 20:29:51 -0700

    mvp complete

---
+++ hobbits/hobbitsModel.js
@@ -0,0 +1,32 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+  insert,
+  update,
+  remove,
+  getAll,
+  findById,
+};
+
+async function insert(hobbit) {
+  const [id] = await db('hobbits').insert(hobbit, 'id');
+  return db('hobbits').where({ id }).first()
+}
+
+async function update(id, changes) {
+  return null;
+}
+
+function remove(id) {
+  return db("hobbits")
+    .where("id", id)
+    .del();
+}
+
+function getAll() {
+  return db('hobbits');
+}
+
+function findById(id) {
+  return null;
+}
